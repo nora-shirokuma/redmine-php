5c0df05bc3353a5ad7dfb11f81031944eb27b9cc
nora-shirokuma <myself@nora-shirokuma.com>
2024-05-07 19:10:32 +0900

    バリデーション追加

--- src/Domain/Redmine/Issue/Subject.php
+++ src/Domain/Redmine/Issue/Subject.php
@@ -2,9 +2,21 @@
 
 namespace NoraShirokuma\RedminePhp\Domain\Redmine\Issue;
 
+use Exception;
 use NoraShirokuma\CommonPhp\AbstractString;
 
 class Subject extends AbstractString
 {
+    protected function validate(?string $value): void
+    {
+        parent::validate($value);
 
+        if (is_null($value)) {
+            throw new Exception('The title must not be NULL.');
+        }
+
+        if ($value === '') {
+            throw new Exception('The title must not be empty.');
+        }
+    }
 }
